77c94e953998b24cca3a5aba42a92f1a4faf620e
Sebastian <sebastian.banfi@gmail.com>
2021-10-14 16:38:00 +0200

    Added some changes to menu animalChoice

--- src/animalgame/Menu.java
+++ src/animalgame/Menu.java
@@ -43,7 +43,7 @@ public class Menu {
 
         switch(ProgramUtils.menuBuilder("Shop","Buy animals","Buy Food","Sell animals")){
             case 1:
-                this.currentGame.getStore().animalToBuy();
+                animalChoice();
                 break;
             case 2:
                 this.currentGame.getStore().foodToBuy();
@@ -56,11 +56,12 @@ public class Menu {
     }
     public void animalChoice(){
 
-        switch(ProgramUtils.menuBuilder("Cat","Dog","Cow","Horse","Snake")){
+        switch(ProgramUtils.menuBuilder("AnimalChoice","cat","Dog","Cow","Horse","Snake")){
             case 1:
-                System.out.println("Choose the animals gender!");
-                ProgramUtils.userInput();
+                String cat = "Cat";
+
         }
+        this.currentGame.getStore().animalToBuy();
     }
 
     public void feedAnimalsMenu(){
