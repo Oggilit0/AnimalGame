e2e4f9229e4583fae2144faa892157813ead788c
Oskar <oskar.herdenberg@gmail.com>
2021-10-18 15:18:43 +0200

    added attribute price in food class, added getter and setter to price

--- src/animalgame/food/abstractmodels/Food.java
+++ src/animalgame/food/abstractmodels/Food.java
@@ -3,6 +3,7 @@ package animalgame.food.abstractmodels;
 public abstract class Food {
     private String name;
     private int weight;
+    private int foodPrice;
 
     public Food(String name, int weight) {
         this.name = name;
@@ -22,6 +23,12 @@ public abstract class Food {
 
     public int getWeight() {
         return weight;
+    }
+    public void setFoodPrice(int foodPrice){
+        this.foodPrice = foodPrice;
+    }
+    public int getFoodPrice(){
+        return this.foodPrice;
     }
 }
 
