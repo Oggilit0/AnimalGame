d229b38c5bbbff9f063ce4da2041b339c87741fb
Sebastian <sebastian.banfi@gmail.com>
2021-10-21 12:16:09 +0200

    mateAnimalMenu changed and fixes animalToBuy

--- src/animalgame/utilities/Menu.java
+++ src/animalgame/utilities/Menu.java
@@ -2,6 +2,7 @@ package animalgame.utilities;
 
 import animalgame.Game;
 import animalgame.Player;
+import animalgame.animals.Cat;
 import animalgame.animals.abstractmodels.Animal;
 import animalgame.enums.Gender;
 import animalgame.utilities.ProgramUtils;
@@ -19,7 +20,7 @@ public class Menu {
 
         int i = 1;
         for(Animal animal : this.currentGame.getCurrentPlayer().getPlayerAnimal()) {
-            System.out.println(i + ".\t" + animal.getClass().getName().substring(11) + " :  namn:" + animal.getName());
+            System.out.println(i + ".\t" + animal.getClass().getName().substring(19) + " :  namn: " + animal.getName()+ " Gender: "+animal.getGender().toString().toLowerCase());
             i++;
         }
     }
@@ -38,7 +39,6 @@ public class Menu {
                 break;
             case 3:
                 mateAnimalsMenu();
-                playerAnimalsAsMenu();
                 break;
             default:
                 roundMenu();
@@ -163,11 +163,20 @@ public class Menu {
     }
 
     public void mateAnimalsMenu(){
-        for(Animal animal : currentGame.getCurrentPlayer().getPlayerAnimal()) {
-            if (currentGame.getCurrentPlayer().getPlayerAnimal().size() < 2) {
-                System.out.println("you need 2 or more animals!");
-            }else{
-                }
-            }
+        if (currentGame.getCurrentPlayer().getPlayerAnimal().size() < 2) {
+            System.out.println("\nyou need 2 or more animals!");
+            System.out.println(currentGame.getCurrentPlayer().getPlayerAnimal().size());
+            roundMenu();
+        }else{
+            playerAnimalsAsMenu();
+            int menuChoice = 0;
+            do {
+                menuChoice = ProgramUtils.tryCatch(ProgramUtils.userInput());
+                System.out.print("Write a number: ");
+
+            }while (menuChoice < 1 || menuChoice > currentGame.getCurrentPlayer().getPlayerAnimal().size());
+            System.out.println("success");
+
         }
+    }
 }
