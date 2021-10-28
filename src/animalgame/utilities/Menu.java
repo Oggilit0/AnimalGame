33f22bfcb4df474430f705912e1b68bc6aedae11
Oskar <oskar.herdenberg@studerande.plushogskolan.se>
2021-10-28 16:47:55 +0200

    Fixed my headache

--- src/animalgame/utilities/Menu.java
+++ src/animalgame/utilities/Menu.java
@@ -165,25 +165,43 @@ public class Menu {
         switch(ProgramUtils.menuBuilder("\nShop","Buy animals","Buy Food","Sell animals")){
             case 1:
                 do{
+
+                    if(this.currentGame.getCurrentPlayer().getMoney() <= 0){
+                        break;
+                    }
                     animalChoice();
-
                 }while(continueMenu("animal","buy"));
-
                 break;
             case 2:
                 do{
+
+                    if(this.currentGame.getCurrentPlayer().getMoney() <= 0){
+                        break;
+                    }
                     shopFoodMenu();
 
                 }while(continueMenu("food","buy"));
 
                 break;
+
             case 3:
-                animalSellMenu();
-             //   do{
-                    // this.currentGame.getStore().animalToSell();
+                boolean test;
+                if(this.currentGame.getCurrentPlayer().getPlayerAnimal().size() == 0){
+                    System.out.println("No animal to sell!");
+                    shopMenu();
+                }else{
+                    do{
+                        boolean test2 = animalSellMenu();
+                        if(test2){
+                            test = continueMenu("animal", "sell");
+                        }else{
+                            test = false;
+                        }
 
-               // }while(continueMenu("animal","sell"));
 
+                    }while(test);
+
+                }
                 break;
             default:
                 shopMenu();
@@ -297,34 +315,19 @@ public class Menu {
 
 
     }
-    public void animalSellMenu() {
-        ArrayList<Animal>animalList = this.currentGame.getCurrentPlayer().getPlayerAnimal();
-        boolean sellCheck = false;
-        do {
-            if (animalList.size() == 0) {
-
-                if(!sellCheck){
-                    System.out.println("You don´t have any animal to sell");
-                    shopMenu();
-                } else {
-                    break;
-                }
-            }
-
-            System.out.println("Choose with animal to sell");
+    public boolean animalSellMenu() {
+        if(this.currentGame.getCurrentPlayer().getPlayerAnimal().size() == 0){
+            System.out.println("No animal to sell!");
+            return false;
+        }else{
+            ArrayList<Animal>animalList = this.currentGame.getCurrentPlayer().getPlayerAnimal();
+            System.out.println("Choose which animal to sell");
             playerAnimalsAsMenu();
             int menuChoice = ProgramUtils.tryCatch(1,animalList.size());
             this.currentGame.getStore().animalToSell(animalList.get(menuChoice-1));
+            return true;
+        }
 
-            if(!(animalList.size() == 0)){
-                System.out.println("Do you want to sell another animal? y/n");
-                if(ProgramUtils.userInput().equalsIgnoreCase("y")){
-                    sellCheck = true;
-                } else {
-                    sellCheck = false;
-                }
-            }
-            } while (sellCheck);
     }
 
     public void feedAnimalsMenu() {
