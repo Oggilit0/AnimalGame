e8b9fea1acb32dac278a1fb2dedcb90324286d61
HaNNa89 <petersson.h@hotmail.com>
2021-10-29 11:11:59 +0200

    added empty JavaDoc

--- src/animalgame/food/Sausage.java
+++ src/animalgame/food/Sausage.java
@@ -1,8 +1,9 @@
 package animalgame.food;
 
 import animalgame.food.abstractmodels.Food;
-
+/** */
 public class Sausage extends Food {
+    /** */
     public Sausage(String name, int weight) {
         super(name, weight);
     }
--- src/animalgame/food/Waffles.java
+++ src/animalgame/food/Waffles.java
@@ -1,8 +1,9 @@
 package animalgame.food;
 
 import animalgame.food.abstractmodels.Food;
-
+/** */
 public class Waffles extends Food {
+    /** */
     public Waffles(String name, int weight) {
         super(name, weight);
     }
