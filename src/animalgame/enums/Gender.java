25b2dcafb1bf6dd5ef6df20b94b058adab0f68f6
Oskar <oskar.herdenberg@studerande.plushogskolan.se>
2021-10-29 16:50:32 +0200

    fixed typo

--- src/animalgame/enums/Gender.java
+++ src/animalgame/enums/Gender.java
@@ -1,7 +1,7 @@
 package animalgame.enums;
 
 /**
- *This is enum class is where we store our gender of animals as enums.
+ * This enum class is where we store our gender of animals as enums.
  * @author Sebastian Banfi, Oskar Herdenberg, Mathilda Nilsson, Hanna Petersson
  */
 public enum Gender {
